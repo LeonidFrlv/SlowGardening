e38d03b35150ae3a9f567a533e2b2fd917f1b8b2
LeonidFrlv <pro-kapr@mail.ru>
2024-08-06 21:55:40 +0300

    1.2. Ягодные кусты и светящиеся ягоды теперь собираются иначе.

--- src/main/java/org/s1queence/plugin/classes/GardenProcess.java
+++ src/main/java/org/s1queence/plugin/classes/GardenProcess.java
@@ -2,6 +2,8 @@ package org.s1queence.plugin.classes;
 
 import org.bukkit.*;
 import org.bukkit.block.Block;
+import org.bukkit.block.data.Ageable;
+import org.bukkit.block.data.type.CaveVinesPlant;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -66,7 +68,26 @@ public class GardenProcess extends CountDownAction {
 
                 if (!getPreprocessActionHandlers().containsKey(player)) {
                     if (gat.equals(GardenActionType.HARVESTING)) {
-                        block.breakNaturally();
+
+                        boolean isSweetBerries = blockType.equals(Material.SWEET_BERRY_BUSH);
+                        if (isSweetBerries || blockType.equals(Material.CAVE_VINES_PLANT)) {
+                            if (isSweetBerries) {
+                                Ageable ageable = (Ageable) block.getBlockData();
+                                ageable.setAge(ageable.getMaximumAge() - 2);
+                                block.setBlockData(ageable);
+                            } else {
+                                CaveVinesPlant caveVinesPlant = (CaveVinesPlant) block.getBlockData();
+                                caveVinesPlant.setBerries(false);
+                                block.setBlockData(caveVinesPlant);
+                            }
+
+                            World world = block.getWorld();
+                            world.dropItemNaturally(blockLocation, new ItemStack(isSweetBerries ? Material.SWEET_BERRIES : Material.GLOW_BERRIES , 3));
+                            world.playSound(blockLocation, isSweetBerries ? Sound.BLOCK_SWEET_BERRY_BUSH_PICK_BERRIES : Sound.BLOCK_CAVE_VINES_PICK_BERRIES, 1.0f,1.0f);
+                        } else {
+                            block.breakNaturally();
+                        }
+
                         setItemDamage(launchItem, player, 1);
                     }
 
